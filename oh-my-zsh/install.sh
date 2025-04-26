697efc6b109c35d7a972fd96d827a8e6866d2a51
abaidal <andres.baidal@eventim.es>
2025-04-26 19:14:34 +0200

    fix ohmyzsh installation

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -6,12 +6,18 @@ IFS=$'\n\t'
 if [[ ! -d ~/.oh-my-zsh ]]; then
     export RUNZSH=no
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+fi
+
+if [[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
 if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]]; then
-    echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
+    POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
     touch ~/.p10k.zsh
     cp "$DOTFILES_ROOT/plugins/dotfiles-walter-baidal/p10k/.p10k.zsh" ~/.p10k.zsh
