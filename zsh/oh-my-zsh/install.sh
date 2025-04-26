3c3dba4399d4c194346405fcc1f9d71e533254ef
abaidal <andres.baidal@eventim.es>
2025-04-26 18:20:41 +0200

    add p10k

--- zsh/oh-my-zsh/install.sh
+++ zsh/oh-my-zsh/install.sh
@@ -8,4 +8,6 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+fi
--- zsh/oh-my-zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh/oh-my-zsh.zsh
@@ -1,10 +1,17 @@
 export ZSH=$HOME/.oh-my-zsh
 export ZSH_DISABLE_COMPFIX="true"
 
-ZSH_THEME=""
-# ZSH_THEME="powerlevel10k/powerlevel10k"
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
 HIST_STAMPS="dd.mm.yyyy"
 plugins=(z git zsh-autosuggestions zsh-syntax-highlighting)
+
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 # If Homebrew is installed under /opt/homebrew folder
 if [ -f /opt/homebrew/bin/brew ]; then
